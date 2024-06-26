48de9e5e37ebf52545b8eb3ece3c7459629ae053
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-06-26 19:24:44 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { lastValueFrom } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-internationalization';
+
+  currentLanguage: string = 'en';
+  constructor(
+    private translate: TranslateService,
+    private apiService: ApiService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.setLanguage();
+  }
+
+  async setLanguage(): Promise<void> {
+    const ipInfo$ = this.apiService.getIPInfo();
+    const ipInfo = await lastValueFrom(ipInfo$);
+
+    this.translate.setDefaultLang('en');
+    if (ipInfo?.country_code?.toUpperCase() == 'BR') {
+      this.translate.setDefaultLang('pt');
+      this.currentLanguage = 'pt';
+    }
+  }
+
+  changeLanguage(): void {
+    if (this.currentLanguage == 'en') {
+      this.translate.use('pt');
+      this.currentLanguage = 'pt';
+    } else {
+      this.translate.use('en');
+      this.currentLanguage = 'en';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
